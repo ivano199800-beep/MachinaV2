75ffbfbd0d652522117d5acc00e42ef2a8bca35f
IVANIK0 <ivano199800@gmail.com>
2026-05-18 16:01:25 +0800

    Implement updateInterface method and enhance tick functionality; add addGate method for gate management

--- machinaV2.hpp
+++ machinaV2.hpp
@@ -62,17 +62,23 @@
             public:
             std::vector<SIGNAL*> interface_wires;
             std::map<size_t , SIGNAL*> wire_mapping;
-            void updateInterface() {
-
+            void updateInterface(size_t wire_index) {
+                if (!wire_mapping.count(wire_index)) return;
+                // currently working on this
+                *(wire_mapping[wire_index]) = internal_wires[wire_index]; 
             }
             void tick() {
                 if (!commited) return;
                 WIP
                 // temporary behavior
                 for (auto& g : internal_gates) {
-                    this->updateInterface();
-                    if (g.invert) PMOS(internal_wires[g.source], internal_wires[g.gate], &internal_wires[g.drain]);
-                    else NMOS(internal_wires[g.source], internal_wires[g.gate], &internal_wires[g.drain]);
+                    this->updateInterface(g.source);
+                    this->updateInterface(g.drain);
+                    this->updateInterface(g.gate);
+                    if (g.invert) 
+                    PMOS(internal_wires[g.source], internal_wires[g.gate], &internal_wires[g.drain]);
+                    else 
+                    NMOS(internal_wires[g.source], internal_wires[g.gate], &internal_wires[g.drain]);
                 }
             }
             void commit() {
@@ -81,13 +87,27 @@
             }
             void map(SIGNAL* interface , size_t internal_index) {
                 if (commited) return;
-                // now working on this
-                WIP
-
+                if (internal_wires.size() < internal_index) return;
+                wire_mapping[internal_index] = interface;
             }
             void wire(size_t gate_index , cmos_pin pin , size_t internal_index) {
+                WIP
                 if (commited) return;
                 // TODO LATER
+                if (gate_index > internal_gates.size()) return;
+                if (internal_index > internal_wires.size()) return;
+
+                internal_gates[gate_index];
+            }
+            size_t addGate(size_t drain , size_t control , size_t source ,bool isN) {
+                if (commited) return 0;
+                Gate g;
+                g.invert = isN;
+                g.drain = drain;
+                g.gate = control;
+                g.source = source;
+                internal_gates.push_back(g);
+                return internal_gates.size() - 1;
             }
             virtual void setup(const std::string& format , const std::vector<std::any>& args) = 0;
         };
